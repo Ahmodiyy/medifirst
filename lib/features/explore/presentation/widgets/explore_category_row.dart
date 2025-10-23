c6aa19b5fe13faebddcfbe045372f73f00f9736d
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-10-23 16:57:21 +0100

    remove unimplemented button

--- lib/features/explore/presentation/widgets/explore_category_row.dart
+++ lib/features/explore/presentation/widgets/explore_category_row.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:medifirst/core/theming/palette.dart';
 import 'package:medifirst/core/widgets/molecules/oops_dialog.dart';
+import 'package:medifirst/features/explore/presentation/screens/emergency_page.dart';
 import 'package:medifirst/features/location/presentation/screens/doctor_list_screen.dart';
 
 import 'explore_category_item.dart';
@@ -20,7 +21,7 @@ class _ExploreCategoryRowState extends State<ExploreCategoryRow> {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         InkWell(
             onTap: () {
@@ -34,7 +35,19 @@ class _ExploreCategoryRowState extends State<ExploreCategoryRow> {
               bottomColor: Palette.consultationDarkGreen,
             )),
         //InkWell(onTap: ()=>showOopsDialog(context), child: const ExploreCategoryItem(svg: 'assets/icons/explore/drugs.png', title: 'Medicine', topColor: Palette.medicineBlue, bottomColor: Palette.medicineDarkBlue,)),
-        //InkWell(onTap: () => showOopsDialog(context),child: const ExploreCategoryItem(svg: 'assets/icons/explore/emergency.png',title: 'Emergency',topColor: Palette.emergencyRed,bottomColor: Palette.emergencyDarkRed,)),
+        InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const EmergencyPage()));
+            },
+            child: const ExploreCategoryItem(
+              svg: 'assets/icons/explore/emergency.png',
+              title: 'Emergency',
+              topColor: Palette.emergencyRed,
+              bottomColor: Palette.emergencyDarkRed,
+            )),
         //InkWell(onTap: ()=>showOopsDialog(context), child: const ExploreCategoryItem(svg: 'assets/icons/explore/healthcare.png', title: 'Healthcare', topColor: Palette.healthcarePurple, bottomColor: Palette.healthcareDarkPurple,)),
       ],
     );
