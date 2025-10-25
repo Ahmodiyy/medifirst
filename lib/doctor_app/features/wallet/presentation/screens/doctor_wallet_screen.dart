13ae34002b6e082672b3f64d3e632d891d0f3531
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-10-25 08:26:36 +0100

    spacing

--- lib/doctor_app/features/wallet/presentation/screens/doctor_wallet_screen.dart
+++ lib/doctor_app/features/wallet/presentation/screens/doctor_wallet_screen.dart
@@ -3,15 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:medifirst/core/theming/spaces.dart';
 import 'package:medifirst/core/widgets/elements/section_heading_text.dart';
 import 'package:medifirst/core/widgets/molecules/doctor_wallet_balance_card.dart';
-import 'package:medifirst/core/widgets/molecules/patient_wallet_balance_card.dart';
 import 'package:medifirst/doctor_app/features/transactions/presentation/screens/doctor_transactions_history_screen.dart';
 import 'package:medifirst/features/auth/controller/auth_controller.dart';
 import 'package:medifirst/features/feedback_and_complaints/presentation/screens/report_issue_screen.dart';
 import 'package:medifirst/features/settings/presentation/widgets/setting_button_tile.dart';
-import 'package:medifirst/features/transactions/presentation/screens/transaction_history_screen.dart';
 
 import '../../../../../core/theming/palette.dart';
-
 
 class DoctorWalletScreen extends ConsumerStatefulWidget {
   const DoctorWalletScreen({super.key});
@@ -42,23 +39,35 @@ class _DoctorWalletScreenState extends ConsumerState<DoctorWalletScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              (size.height * 24/852).pv,
+              (size.height * 24 / 852).pv,
               const DoctorWalletBalanceCard().sidePad(16),
-              (size.height * 30/852).pv,
-              const SectionHeadingText(heading: 'SETTINGS').sidePad(16).alignLeft(),
-              (size.height * 12/852).pv,
+              (size.height * 30 / 852).pv,
+              const SectionHeadingText(heading: 'SETTINGS')
+                  .sidePad(16)
+                  .alignLeft(),
+              (size.height * 12 / 852).pv,
               InkWell(
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const DoctorTransactionHistoryScreen()));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const DoctorTransactionHistoryScreen()));
                 },
                 child: const SettingButtonTile(label: 'Transaction History'),
               ),
               InkWell(
                   onTap: () {
-                    Navigator.push(context, (MaterialPageRoute(builder: (context)=>const ReportIssueScreen())));
+                    Navigator.push(
+                        context,
+                        (MaterialPageRoute(
+                            builder: (context) => const ReportIssueScreen())));
                   },
-                  child: const SettingButtonTile(label: 'Report a problem', svg: 'assets/icons/svgs/danger.svg', color: Palette.redTextColor,)
-              ),
+                  child: const SettingButtonTile(
+                    label: 'Report a problem',
+                    svg: 'assets/icons/svgs/danger.svg',
+                    color: Palette.redTextColor,
+                  )),
             ],
           ),
         ),
